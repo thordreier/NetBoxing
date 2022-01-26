e43cf99cda50b963fb102fc88ecdc32fa97c89b4
Thor Dreier-Hansen <43479811+thordreier@users.noreply.github.com>
2022-01-26 08:41:15 +0100

    Add help

--- EXAMPLES.ps1
+++ EXAMPLES.ps1
@@ -1,12 +1,3 @@
-# NetBoxing
-
-PowerShell functions to interact with NetBox API.
-
-## Usage
-
-### Examples
-
-```powershell
 #### Connect-Netbox
 
 # Connect to Netbox
@@ -156,27 +147,3 @@ foreach ($site in $sites)
         }
     }
 }
-
-```
-
-Examples are also found in [EXAMPLES.ps1](EXAMPLES.ps1).
-
-### Functions
-
-See [FUNCTIONS.md](FUNCTIONS.md) for documentation of funtions in this module.
-
-## Install
-
-### Install module from PowerShell Gallery
-
-```
-Install-Module NetBoxing
-```
-
-### Install module from source
-
-```
-git clone https://github.com/thordreier/NetBoxing.git
-cd NetBoxing
-.\Build.ps1 -InstallModule
-```
