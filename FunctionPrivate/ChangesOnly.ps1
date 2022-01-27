ca096d6586da5b7ac7f7511cdf1ddc75deeafa0c
Thor Dreier-Hansen <43479811+thordreier@users.noreply.github.com>
2022-01-27 12:43:37 +0100

    Add array functionality to diff

--- FunctionPrivate/ChangesOnly.ps1
+++ FunctionPrivate/ChangesOnly.ps1
@@ -1,25 +1,93 @@
 function ChangesOnly ([PSObject] $Orig, [hashtable] $Changes)
 {
+    function NotIdentical ($Orig, $Changes)
+    {
+        ($Changes -is [hashtable] -and (ChangesOnly -Orig $Orig -Changes $Changes).Count) -or
+        ($Changes -isnot [hashtable] -and $Orig -cne $Changes)
+    }
+
     $expand = $null
     $Changes = $Changes.Clone()
-    @($Changes.Keys) | ForEach-Object -Process {
-        if ($_ -ceq '___EXPAND')
+    foreach ($key in @($Changes.Keys))
+    {
+        if ($key -ceq '___EXPAND')
         {
-            $expand = $Changes.$_
-            $null = $Changes.Remove($_)
+            $expand = $Changes.$key
+            $null = $Changes.Remove($key)
         }
-        elseif ($Changes.$_ -is [hashtable])
+        elseif ($Changes.$key -is [hashtable])
         {
-            if (-not ($Changes.$_ = ChangesOnly -Orig $Orig.$_ -Changes $Changes.$_).Count)
+            if (-not ($Changes.$key = ChangesOnly -Orig $Orig.$key -Changes $Changes.$key).Count)
             {
-                $Changes.Remove($_)
+                $Changes.Remove($key)
+            }
+        }
+        elseif ($Changes.$key -is [array])
+        {
+            if ($Changes.$key.Count -and $Changes.$key[0] -is [hashtable] -and $Changes.$key[0].___APPEND)
+            {
+                $append, $Changes.$key = $Changes.$key
+                if (($ok = $Orig.$key) -isnot [array])
+                {
+                    $ok = @()
+                }
+                $identical = $true
+                $combined = @($ok | Select-Object -Property $append.___APPEND)
+                foreach ($c in $Changes.$key)
+                {
+                    $exist = $false
+                    foreach ($o in $ok)
+                    {
+                        if (-not (NotIdentical -Orig $o -Changes $c))
+                        {
+                            $exist = $true
+                            break
+                        }
+                    }
+                    if (-not $exist)
+                    {
+                        $combined += $c
+                        $identical = $false
+                    }
+                }
+                if ($identical)
+                {
+                    $Changes.Remove($key)
+                }
+                else
+                {
+                    $Changes.$key = $combined
+                }
+            }
+            else
+            {
+                if ($Orig.$key -is [array] -and $Orig.$key.Count -eq $Changes.$key.Count)
+                {
+                    $identical = $true
+                    for ($i=0; $i -lt $Changes.$key.Count; $i++)
+                    {
+                        if (NotIdentical -Orig $Orig.$key[$i] -Changes $Changes.$key[$i])
+                        {
+                            $identical = $false
+                            break
+                        }
+                    }
+                    if ($identical)
+                    {
+                        $Changes.Remove($key)
+                    }
+                }
+                elseif ($Orig.$key -eq $null -and $Changes.$key.Count -eq 0)
+                {
+                    $Changes.Remove($key)
+                }
             }
         }
         else
         {
-            if ($Orig.$_ -ceq $Changes.$_)
+            if ($Orig.$key -ceq $Changes.$key)
             {
-                $null = $Changes.Remove($_)
+                $null = $Changes.Remove($key)
             }
         }
     }
